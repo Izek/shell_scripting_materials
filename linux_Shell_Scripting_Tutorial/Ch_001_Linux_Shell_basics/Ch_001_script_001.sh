a1dd8b8b24d6e9690b41cd270427b184007ea61f
Izek <izek.schum@gmail.com>
2013-07-07 13:05:40 +0300

    0000. LSST is started on separate branch

---
+++ linux_Shell_Scripting_Tutorial/Ch_001_Linux_Shell_basics/Ch_001_script_001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+
+echo "This is a 1st script of LSST =)"
+echo
+
+echo "There are following sheels in current system: "
+cat /etc/shells
+
+echo
+echo "Current shell is: "; echo $SHELL
+
+echo
+echo "Done!.."
+echo
+echo
